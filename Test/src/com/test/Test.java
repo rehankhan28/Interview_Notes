628d9e91e4b05e63c0c47c72f63ff6e8ee27d496
Your Name <you@example.com>
2024-07-30 23:26:46 +0530

    reason for commit

--- Test/src/com/test/Test.java
+++ Test/src/com/test/Test.java
@@ -1,13 +1,4 @@
 package com.test;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-
 class Test {
-	public static void main(String arg[]) {
-		List<Integer> list = Arrays.asList(12, 21, 23, 33, 34343, 12, 21);
-		List<Integer> sortedListAsc = list.stream().distinct().sorted().collect(Collectors.toList());
-		System.out.println(sortedListAsc);
-	}
 }
